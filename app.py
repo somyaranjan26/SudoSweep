048354703a9e677ca1fe15ac5004961a78a71a18
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2020-07-07 04:48:19 +0530

    Updated Covid files

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
+CURRENT_DIRECTORY    = os.path.dirname( os.path.dirname( os.path.realpath( __file__ ) ) )
+CURRENT_PACKAGE_NAME = os.path.basename( CURRENT_DIRECTORY ).rsplit('.', 1)[0]
+
 UPLOAD_FOLDER = './flask app/assets/images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 # Create Database if it doesnt exist
@@ -30,9 +33,13 @@ def upload():
 def upload_chest():
    return render_template('upload_covid.html')
 
-@app.route('/upload_ct.html')
-def upload_ct():
-   return render_template('upload_ct.html')
+@app.route('/upload_covidAndNormal.html')
+def upload_covid():
+   return render_template('upload_covid.html')
+
+@app.route('/upload_PneumoniaAndNormal.html')
+def upload_pneumonia():
+   return render_template('upload_covid.html')
 
 
 @app.route('/uploaded_covidAndPneumonia', methods = ['POST', 'GET'])
@@ -85,9 +92,8 @@ def uploaded_chest():
    return render_template('results_covid.html',vgg_chest_pred=vgg_chest_pred, colour=colour, status=status, message=message, result=result)
 
 
-
-@app.route('/uploaded_ct', methods = ['POST', 'GET'])
-def uploaded_ct():
+@app.route('/uploaded_covidAndNormal', methods = ['POST', 'GET'])
+def uploaded_covid():
    if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -101,38 +107,91 @@ def uploaded_ct():
             return redirect(request.url)
         if file:
             # filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_ct.jpg'))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
 
-   resnet_ct = load_model('models/resnet_ct.h5')
-   vgg_ct = load_model('models/vgg_ct.h5')
+   # resnet_chest = load_model('')
+   vgg_chest = load_model('models/vgg_covid100.h5')
 
-   image = cv2.imread('./flask app/assets/images/upload_ct.jpg') # read file 
+   image = cv2.imread('./flask app/assets/images/upload_chest.jpg') # read file 
    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
    image = cv2.resize(image,(224,224))
    image = np.array(image) / 255
    image = np.expand_dims(image, axis=0)
    
-   # resnet_pred = resnet_ct.predict(image)
-   # probability = resnet_pred[0]
-   # print("Resnet Predictions:")
-   # if probability[0] > 0.5:
-   #    resnet_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
-   # else:
-   #    resnet_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
-   # print(resnet_ct_pred)
-
-   vgg_pred = vgg_ct.predict(image)
+   vgg_pred = vgg_chest.predict(image)
    probability = vgg_pred[0]
    print("VGG Predictions:")
+   colour = ""
+   status = ""
+   message= ""
+   result = ""
    if probability[0] > 0.5:
-      vgg_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+      vgg_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+      colour = "danger"
+      status = "COVID-19"
+      message="SudoSweep found it "
+      result = "COVID19 Positive "
    else:
-      vgg_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
-   print(vgg_ct_pred)
+      vgg_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% Non-COVID')
+      colour = "success"
+      status = "Normal"
+      message="SudoSweep found it "
+      result = "Normal "
+   print(vgg_chest_pred)
+
+   return render_template('results_covid.html',vgg_chest_pred=vgg_chest_pred, colour=colour, status=status, message=message, result=result)
 
 
-   return render_template('results_ct.html', vgg_ct_pred=vgg_ct_pred)
+@app.route('/uploaded_PneumoniaAndNormal', methods = ['POST', 'GET'])
+def uploaded_pneumonia():
+   if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+
+   # resnet_chest = load_model('')
+   vgg_chest = load_model('models/keras_model.h5')
+
+   image = cv2.imread('./flask app/assets/images/upload_chest.jpg') # read file 
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+   image = cv2.resize(image,(224,224))
+   image = np.array(image) / 255
+   image = np.expand_dims(image, axis=0)
+   
+   vgg_pred = vgg_chest.predict(image)
+   probability = vgg_pred[0]
+   print("VGG Predictions:")
+   colour = ""
+   status = ""
+   message= ""
+   result = ""
+   if probability[0] > 0.5:
+      vgg_chest_pred = str('%.2f' % (probability[0]*100) + '% PNEUMONIA') 
+      colour = "danger"
+      status = "Pneumonia"
+      message="SudoSweep found it "
+      result = "Pneumonia Positive "
+   else:
+      vgg_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NON-PNEUMONIA')
+      colour = "success"
+      status = "Normal"
+      message="SudoSweep found it "
+      result = "Normal "
+   print(vgg_chest_pred)
+
+   return render_template('results_covid.html',vgg_chest_pred=vgg_chest_pred, colour=colour, status=status, message=message, result=result)
+
 
 if __name__ == '__main__':
    app.secret_key = ".."
-   app.run()
+   app.run(host="0.0.0.0", port=80)
